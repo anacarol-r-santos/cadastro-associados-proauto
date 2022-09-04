a9c8379d0d920073279fb0b2c834fb6705b58883
Carol <kroline0693@gmail.com>
2022-09-04 19:22:16 -0300

    Adiciona validação para cadastro

--- backend/controllers/cadastro.js
+++ backend/controllers/cadastro.js
@@ -1,15 +1,24 @@
-// const { schema } = require('../services/validacaoPost');
+const { schema } = require('../services/validacaoPost');
 const { Associado } = require('../models');
 
 const postAssociado = async (req, res) => {
+    const { nome, cpf, placa, endereco, telefone } = req.body;
+
+    const { error } = schema.validate(
+        { nome, cpf, placa, endereco, telefone },
+        { abortEarly: false },
+    );
+    if (error) {
+        return res.status(400).json(error.message);
+    }
+
     try {
-        const { nome, cpf, placa, endereco, telefone } = req.body;
         const novoAssociado = await Associado.create({
             nome, cpf, placa, endereco, telefone
         });
         return res. status(201).json(novoAssociado);
     } catch (error) {
-        console.log(e.message);
+        console.log(error.message);
         res.status(500).json({ message: 'Algo deu errado' });
     }
 }
--- backend/services/validacaoPost.js
+++ backend/services/validacaoPost.js
@@ -1,11 +1,31 @@
 const Joi = require('joi');
 
 const schema = Joi.object().keys({
-    nome: Joi.string().empty().required(),
-    cpf: Joi.string().required().empty(),
-    placa: Joi.string().required().empty(),
-    endereco: Joi.string().required().empty(),
+    nome: Joi.string().empty().required()
+    .messages({
+      'string.empty': 'Nome não foi informado',
+      'any.required': 'Nome não foi informado',
+    }),
+    cpf: Joi.string().required().empty()
+    .messages({
+      'string.empty': 'CPF não foi informado',
+      'any.required': 'CPF não foi informado',
+    }),
+    placa: Joi.string().required().empty()
+    .messages({
+      'string.empty': 'Placa não foi informada',
+      'any.required': 'Placa não foi informada',
+    }),
+    endereco: Joi.string().required().empty()
+    .messages({
+      'string.empty': 'Endereço não foi informado',
+      'any.required': 'Endereço não foi informado',
+    }),
     telefone: Joi.string().required().empty()
+    .messages({
+      'string.empty': 'Telefone não foi informado',
+      'any.required': 'Telefone não foi informado',
+    }),
   });
 
 module.exports = { schema };
