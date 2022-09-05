9de04ea4a3c1423cd89c616f7dc4e95d9f5d0a4a
Carol <kroline0693@gmail.com>
2022-09-05 18:59:47 -0300

    Implementa arquivo para rotas

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
-const { postAssociado } = require('../controllers/cadastro');
+const { postAssociado } = require('../controllers/cadastraAssociado');
 const { login } = require('../controllers/login');
 const { validaToken } = require('../controllers/auth');
+const { dadosAssociado } = require('../controllers/buscaAssociado');
+const { editaTelefone } = require('../controllers/alteraTelefone');
+const { editaEndereco } = require('../controllers/alteraEndereco');
 
 router.post('/cadastro', postAssociado);
-router.post('/login', validaToken, login);
+router.post('/login', login);
+router.get('/:id', validaToken, dadosAssociado);
+router.put('/telefone/:id', validaToken, editaTelefone);
+router.put('/endereco/:id', validaToken, editaEndereco);
 
 module.exports = { router };
